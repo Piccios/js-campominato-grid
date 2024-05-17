c999b5fe94061893a5a5ee2eea345f582d430996
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-17 16:44:27 +0200

    created 100 boxes inside the grid, with JS

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,11 @@
 const gridElement = document.querySelector('section#grid_100');
 
+
+
 /* <article class="square"></article> */
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let index = 0; index < 100; index++) {
+    const squareElement = document.createElement('article'); // che sara' il mio <article>
+    squareElement.classList.add('square');
+    gridElement.appendChild(squareElement);
 }
