89d554ad1a8833634885ac0ccbe7556c006a4b17
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-17 16:35:01 +0200

    created a const selecting the section#grid

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+const gridElement = document.querySelector('section#grid_100');
+
+/* <article class="square"></article> */
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
